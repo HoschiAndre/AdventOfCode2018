fd11d2def4ced21d016697c1cf1423fa9535e8ce
Q237017 <andre.kisters@bmw.de>
2018-11-30 11:15:49 +0100

    Initial Setup

---
+++ AdventOfCode01/src/AdventOfCode01.java
@@ -0,0 +1,8 @@
+public class AdventOfCode01 {
+
+    public void start() {
+        System.out.println("Starting ...");
+
+    }
+
+}
---
+++ AdventOfCode01/src/AdventOfCode01Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+
+public class AdventOfCode01Test {
+
+    @Test
+    public void startTest() {
+        AdventOfCode01 adventOfCode01 = new AdventOfCode01();
+
+        adventOfCode01.start();
+
+    }
+
+}
