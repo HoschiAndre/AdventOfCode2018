435b96fb9e840f642ce86f634a64c3e8cb137d6f
HoschiAndre <hoschi.andre@gmail.com>
2018-12-01 18:36:47 +0100

    Day 01

--- AdventOfCode01/src/AdventOfCode01.java
+++ AdventOfCode01/src/AdventOfCode01.java
@@ -1,8 +1,33 @@
+import java.util.*;
+
 public class AdventOfCode01 {
 
-    public void start01() {
+    public int start01(String input) {
         System.out.println("Starting Advent of Code 01 ...");
 
+        List<String> inputList = new ArrayList<>(Arrays.asList(input.split("\n")));
+        Set<Integer> resultTwice = new HashSet<>();
+
+        int result = 0;
+        boolean twice = true;
+        int i = 0;
+
+        for (; ; ) {
+
+            if (i == inputList.size()) {
+                i = 0;
+            }
+
+            result = result + Integer.parseInt(inputList.get(i));
+
+            twice = resultTwice.add(result);
+
+            if (twice == false) {
+                return result;
+            }
+
+            i++;
+        }
     }
 
 }
